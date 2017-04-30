2f7dd8f13d1c53e1463e09110fab3206e1529cd6
Daniel Garfias <daniel.garfias@pcc.edu>
2017-04-29 19:57:59 -0700

    Added in a few simple print statements to the main method

--- src/HelloCIS/HelloCIS.java
+++ src/HelloCIS/HelloCIS.java
@@ -1,7 +1,18 @@
 package HelloCIS;
 
 /**
- * Created by danga on 4/29/2017.
+ * This is my project for week 4. It currently only
+ * has a few simple print statements.
+ * Author: Daniel Garfias
+ * Date: 4-29-17
  */
+
 public class HelloCIS {
+    public static void main(String[] args) {
+        System.out.println("Hello CIS 233");
+        System.out.println("This is my first time working with IntelliJ and Git so everything is still a little weird.");
+        System.out.println("Git bash completely new to me and I'm still wrapping my head around it");
+        System.out.println("As I spend more time with these new programs, I should be fine");
+
+    }
 }
