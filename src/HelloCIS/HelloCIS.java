71edbda352e81282f46c45cc42c610fa089a402f
Daniel Garfias <daniel.garfias@pcc.edu>
2017-04-29 20:54:40 -0700

    Another change to a print statment

--- src/HelloCIS/HelloCIS.java
+++ src/HelloCIS/HelloCIS.java
@@ -13,7 +13,7 @@ public class HelloCIS {
         System.out.println("This is my first time working with IntelliJ and Git so everything is still a little weird.");
         System.out.println("Git bash completely new to me and I'm still wrapping my head around it");
         System.out.println("As I spend more time with these new programs, I should be fine");
-        System.out.println("This print statement should be part of the playing around branch");
+        System.out.println("It might take a while for me to get the hang of commits, pushes and pulls");
         System.out.println("I'm messing around with branches as much as I can because I have the feeling I'm doing something wrong.");
         System.out.println("This is just an extara line for the sake of change");
 
